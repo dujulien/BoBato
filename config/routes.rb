14f4315fa3da3e20a322fd3c13f269ae91469b5a
YoucefKhalainou <youcef_khalainou@yahoo.fr>
2019-12-03 11:27:02 +0100

    show_profile_boat_owner_skipper

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'demo#index'
-  resources :demo
+  # root 'demo#index'
+  # resources :demo
+  root 'boat_owner#show'
+  # root 'skipper#show'
 end
