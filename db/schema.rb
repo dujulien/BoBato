5c87ce8daf18ed7ad62d468965a4967f6cafbb09
dujulien <du.julien1@gmail.com>
2019-12-03 01:22:41 +0100

    Title and description attributes added to Convoy table. Migration + seed reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_12_02_220203) do
 
   # These are extensions that must be enabled in order to support this database
@@ -44,8 +43,10 @@ ActiveRecord::Schema.define(version: 2019_12_02_220203) do
 
   create_table "convoys", force: :cascade do |t|
     t.bigint "boat_owner_id"
+    t.string "title"
     t.string "boat_type"
     t.string "required_license"
+    t.text "description"
     t.string "departure_port"
     t.string "arrival_port"
     t.datetime "date_of_departure"
@@ -83,7 +84,18 @@ ActiveRecord::Schema.define(version: 2019_12_02_220203) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_skippers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_skippers_on_reset_password_token", unique: true
+  end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
