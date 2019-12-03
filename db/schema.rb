72534506382e2585d0b8953e5a7c4e805f855ab4
dujulien <du.julien1@gmail.com>
2019-12-03 17:54:19 +0100

    [WIP] Login modal

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,8 @@
 
 ActiveRecord::Schema.define(version: 2019_12_03_140904) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -37,7 +35,6 @@ ActiveRecord::Schema.define(version: 2019_12_03_140904) do
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
-
 
   create_table "boat_owners", force: :cascade do |t|
     t.string "first_name"
@@ -110,5 +107,5 @@ ActiveRecord::Schema.define(version: 2019_12_03_140904) do
     t.index ["skipper_id"], name: "index_submissions_on_skipper_id"
   end
 
-
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
