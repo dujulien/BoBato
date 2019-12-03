1ffaa9de2e71d7595bbde1feef91e19c85c74d29
Yoni <y_benatar@hotmail.com>
2019-12-03 14:23:43 +0100

    Welcome mailer added with dev-letter_opener

--- app/models/boat_owner.rb
+++ app/models/boat_owner.rb
@@ -3,4 +3,12 @@ class BoatOwner < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_boatowner_email(self).deliver_now
+  end
+
 end
