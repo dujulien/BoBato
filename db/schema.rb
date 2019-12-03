cd30a052a875263b7466fec7d9239e380339ce7f
ombelinedej <ombeline.dj@gmail.com>
2019-12-03 16:57:14 +0100

    nav + footer

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_12_02_220203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -37,8 +35,6 @@ ActiveRecord::Schema.define(version: 2019_12_02_220203) do
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
-
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 
   create_table "applications", force: :cascade do |t|
     t.bigint "skipper_id"
@@ -123,5 +119,5 @@ ActiveRecord::Schema.define(version: 2019_12_02_220203) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
