07dcea5c707128e539ceabdef4446df3418f3469
Marius-creator <marius.martinez40@gmail.com>
2019-12-06 18:27:03 +0100

    show and model

--- app/models/convoy.rb
+++ app/models/convoy.rb
@@ -1,41 +1,24 @@
 class Convoy < ApplicationRecord
-<<<<<<< HEAD
-
-
+	
   has_many_attached :pictures
 
-=======
-	has_one_attached :picture
->>>>>>> 6cbbab63aae66ea520a311ad68887991458b2a1b
 	belongs_to :boat_owner, class_name: 'User'
 	has_one :delivery
 	has_many :submissions
 	has_many :applicants, through: :submissions, :source => :skipper
 
-	# validates :date_of_departure, presence: { message: "Il n'y a pas de date de départ"}
-  # validates :date_of_arrival, presence: { message: "Il n'y a pas de date d'arrivée"}
-  # validates :convoy_price, presence: { message: "Il n'y a pas de prix"}
-  # validates :convoy_price, numericality: { greater_than: 0 , message: "Le prix doit être supérieur à 0"}
-  # validate :departure_must_be_in_future
-  # validate :departure_must_be_before_arrival
+	validates :date_of_departure, presence: { message: "Il n'y a pas de date de départ"}
+  validates :date_of_arrival, presence: { message: "Il n'y a pas de date d'arrivée"}
+  validates :convoy_price, presence: { message: "Il n'y a pas de prix"}
+  validates :convoy_price, numericality: { greater_than: 0 , message: "Le prix doit être supérieur à 0"}
+  validate :departure_must_be_in_future
+  validate :departure_must_be_before_arrival
 
-  after_create :convoy_conf_email_send,
-
-
-  def duration
-    ((self.date_of_arrival - self.date_of_departure + 1)/(60*60*24)).to_i
-  end
-
-
-  def convoy_conf_email_send
-    UserMailer.convoy_conf_email(self).deliver_now
-  end
-
+	private
 
 	def departure_must_be_in_future
     errors.add(:date_of_departure, "La date de départ doit être supérieure à la date du jour") if date_of_departure < Time.now
   end
-
 
 	def departure_must_be_before_arrival
 	  return unless date_of_departure and date_of_arrival
