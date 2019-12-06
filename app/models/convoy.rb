b1588772567e2a1a4b25370304b6a337652d5c12
Yoni <y_benatar@hotmail.com>
2019-12-06 10:11:19 +0100

    Models validation update for skipper availability check before submission and delivery

--- app/models/convoy.rb
+++ app/models/convoy.rb
@@ -9,20 +9,20 @@ class Convoy < ApplicationRecord
   validates :date_of_arrival, presence: { message: "Il n'y a pas de date d'arrivée"}
   validates :convoy_price, presence: { message: "Il n'y a pas de prix"} 
   validates :convoy_price, numericality: { greater_than: 0 , message: "Le prix doit être supérieur à 0"}
-  validate :departure_must_be_before_arrival
   validate :departure_must_be_in_future
+  validate :departure_must_be_before_arrival
 
 
 	private
+
+	def departure_must_be_in_future
+    errors.add(:date_of_departure, "La date de départ doit être supérieure à la date du jour") if date_of_departure < Time.now
+  end
 
 	def departure_must_be_before_arrival
 	  return unless date_of_departure and date_of_arrival
 	  errors.add(:date_of_departure, "La date de départ doit être avant la date d'arrivée") unless date_of_departure < date_of_arrival
 	end
-
-	def departure_must_be_in_future
-    errors.add(:date_of_departure, "La date de départ doit être supérieure à la date du jour") if date_of_departure < Time.now
-  end
 
 
 end
--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,4 +1,13 @@
 class Delivery < ApplicationRecord
 	belongs_to :skipper, class_name: 'User'
 	belongs_to :convoy
+
+	validate :skipper_must_be_available_for_this_convoy
+
+
+	def skipper_must_be_available_for_this_convoy
+	  errors.add(:skipper_id, "Le skipper choisi n'est pas disponible pour les dates selectionnées") unless check_skipper_availability
+	end
+
+
 end
