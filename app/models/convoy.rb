d407ac1bd91c2c4a34f07a70cde407ff03094496
Yoni <y_benatar@hotmail.com>
2019-12-09 00:05:08 +0100

    Devise mail modifications + Mailer correction

--- app/models/convoy.rb
+++ app/models/convoy.rb
@@ -13,16 +13,22 @@ class Convoy < ApplicationRecord
   validates :convoy_price, numericality: { greater_than: 0 , message: "Le prix doit être supérieur à 0"}
   validate :departure_must_be_in_future
   validate :departure_must_be_before_arrival
-
+  after_create :convoy_conf_email_send,
+  
+  
   def duration
   	(self.date_of_arrival - self.date_of_departure)/(60*60*24).round(0)
   end
 
-	private
+  def convoy_conf_email_send
+    UserMailer.convoy_conf_email(self).deliver_now
+  end
+
 
 	def departure_must_be_in_future
     errors.add(:date_of_departure, "La date de départ doit être supérieure à la date du jour") if date_of_departure < Time.now
   end
+
 
 	def departure_must_be_before_arrival
 	  return unless date_of_departure and date_of_arrival
