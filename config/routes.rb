4dc493ec1678004fd65567dbd74c53f2d436829f
Yoni <y_benatar@hotmail.com>
2019-12-13 17:03:22 +0100

    routes renamed

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,29 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  devise_for :users, path: '', path_names: { sign_in: 'connexion', sign_out: 'deconnexion', password: 'mot_de_passe', sign_up: 'inscription', edit: 'parametres'}
   root 'home#index'
-  devise_for :users
   
   resources :user do
-    resources :my_convoys, only: [:index, :show]
-    resources :submissions, only: [:index]
+    resources :my_convoys, only: [:index, :show], :path => "mes_convoyages"
+    resources :submissions, only: [:index], :path => "mes_candidatures"
   end
 
   resources :home, only: [:index]
 
-  resources :convoys do
+  resources :convoys, :path => "convoyages" do
   	resources :pictures
   end
 
-  resources :deliveries
-  resources :charges
+  resources :deliveries, :path => "convoyages_confirme"
+  resources :charges, :path => "paiement"
   resources :submissions, only: [:create, :destroy]
 
-  resources :profiles do
+  resources :profiles, :path => "mon_profil" do
     resources :avatars, only: [:create]
   end
 
-  resources :comments
+  resources :comments, :path => "commentaires"
 
   # Custom error pages
   get '/404', to: 'errors#not_found'
